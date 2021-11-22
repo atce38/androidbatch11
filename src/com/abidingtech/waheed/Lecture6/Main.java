60c035c7ddb210b176a3a9e2da8a00c08d488adc
atce38 <dadag38@gmail.com>
2021-11-22 12:39:21 +0500

    Lecture 6-1

--- src/com/abidingtech/waheed/Lecture6/Main.java
+++ src/com/abidingtech/waheed/Lecture6/Main.java
@@ -1,4 +1,26 @@
 package com.abidingtech.waheed.Lecture6;
 
+import java.util.ArrayList;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> studentList=new ArrayList<>();
+        Student assvvasd=new Student();
+        assvvasd.name="Hassan";
+        assvvasd.age=23;
+        studentList.add(assvvasd);
+        //assvvasd.result();
+
+
+        Student student1=new Student();
+        student1.name="Usama";
+        student1.age=23;
+//        student1.result();
+        studentList.add(student1);
+
+
+    }
 }
---
+++ src/com/abidingtech/waheed/Lecture6/Student.java
@@ -0,0 +1,11 @@
+package com.abidingtech.waheed.Lecture6;
+
+public class Student {
+    String name;
+    int age;
+
+    void result(){
+        System.out.println("Student Name= "+name);
+        System.out.println("Student Age= "+age);
+    }
+}
